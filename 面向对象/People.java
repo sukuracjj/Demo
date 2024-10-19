abe787028a03d3b249fde6b197081f4152fd0854
sakurajjj <3262169992@qq.com>
2024-10-19 17:07:22 +0800

    r

--- 面向对象/People.java
+++ 面向对象/People.java
@@ -4,10 +4,12 @@ public class People {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         Person a = new Person(18,175,"男");
-        System.out.println("我们该怎么称呼您？");
+        a.benn();
         a.setName(in.nextLine());
         System.out.print(">>已自动生成你的资料<<\n");
-        System.out.println("姓名："+a.getName()+"\n"+"年龄： "+a.age+"\n"+"");
+        System.out.println("姓名："+a.getName()+"\n"+"年龄： "+a.age);
+        System.out.println("性别："+a.sex);
+        System.out.println("身高："+a.Height);
+        in.close();
     }
-    
 }
--- 面向对象/Person.java
+++ 面向对象/Person.java
@@ -1,15 +1,17 @@
 package 面向对象;
 
-public class Person extends Animals{
+public class Person extends Animals{//继承
     public Person(int a,double b,String c){
         super(a,b,c);
     }
-    private String Name;
-    
+    public void benn(){
+        System.out.println("我们该怎么称呼您？");
+    }
+    private String Name; //封装
     public String getName(){
         return Name;
     }
-    public void setName(String Name){
+    public void setName(String Name){//内部类
         this.Name = Name;
     }
 
